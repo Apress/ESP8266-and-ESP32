05f8f386d9c94dd4c1ac32c6538efd20c90aaf54
NDC-SC <38754611+NDC-SC@users.noreply.github.com>
2022-03-19 15:42:20 +0000

    Add files via upload

---
+++ Chapter 21/Listing21-5_NEW/Listing21-5_NEW.ino
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Sketch name: Analog to digital conversion
+ * Description: ADC functionality of ESP32 microcontroller  
+ * Created on:  October 2020
+ * Author:      Neil Cameron
+ * Book:        Electronics Projects with the ESP8266 and ESP32
+ * Chapter :    21 - Microcontrollers
+ ******************************************************************************/
+
+int ADCpin = 36;            // define ADC pin
+
+void setup()
+{
+  Serial.begin(115200);         // define Serial Monitor baud rate
+  Serial.println();
+  analogSetAttenuation(ADC_11db);  // default setting of 11dB
+  Serial.println(analogRead(ADCpin));   // read ADC pin
+  analogSetAttenuation(ADC_6db); // change setting to 6dB
+  Serial.println(analogRead(ADCpin));
+  analogSetAttenuation(ADC_2_5db);
+  Serial.println(analogRead(ADCpin));
+  analogSetAttenuation(ADC_0db);
+  Serial.println(analogRead(ADCpin));
+}
+
+void loop()
+{}                // nothing in loop function
